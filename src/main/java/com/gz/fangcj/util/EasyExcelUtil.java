258842933619f13f144c65ea0075b543a4c37477
fangchenjiang <fangchenjiang@37.com>
2021-01-15 15:10:07 +0800

    文档导出自定义格式

--- src/main/java/com/gz/fangcj/util/EasyExcelUtil.java
+++ src/main/java/com/gz/fangcj/util/EasyExcelUtil.java
@@ -2,7 +2,12 @@ package com.gz.fangcj.util;
 
 import cn.hutool.json.JSONUtil;
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.write.metadata.style.WriteCellStyle;
+import com.alibaba.excel.write.metadata.style.WriteFont;
+import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
 import com.gz.fangcj.entity.Student;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -25,6 +30,27 @@ public class EasyExcelUtil {
      * @param data
      */
     public static void exportExcel(HttpServletResponse response, List  data)throws IOException {
+        // 头的策略
+        WriteCellStyle headWriteCellStyle = new WriteCellStyle();
+        // 背景设置为红色
+        headWriteCellStyle.setFillForegroundColor(IndexedColors.AUTOMATIC.getIndex());
+        WriteFont headWriteFont = new WriteFont();
+        headWriteFont.setFontName("微软雅黑");
+        headWriteFont.setFontHeightInPoints((short) 10);
+        headWriteCellStyle.setWriteFont(headWriteFont);
+        // 内容的策略
+        WriteCellStyle contentWriteCellStyle = new WriteCellStyle();
+        // 这里需要指定 FillPatternType 为FillPatternType.SOLID_FOREGROUND 不然无法显示背景颜色.头默认了 FillPatternType所以可以不指定
+        contentWriteCellStyle.setFillPatternType(FillPatternType.NO_FILL);
+        // 背景绿色
+        contentWriteCellStyle.setFillForegroundColor(IndexedColors.AUTOMATIC.getIndex());
+        WriteFont contentWriteFont = new WriteFont();
+        // 字体大小
+        contentWriteFont.setFontHeightInPoints((short) 12);
+        contentWriteCellStyle.setWriteFont(contentWriteFont);
+        // 这个策略是 头是头的样式 内容是内容的样式 其他的策略可以自己实现
+        HorizontalCellStyleStrategy horizontalCellStyleStrategy =
+                new HorizontalCellStyleStrategy(headWriteCellStyle, contentWriteCellStyle);
         // 这里注意 有同学反应使用swagger 会导致各种问题，请直接用浏览器或者用postman
         try {
             response.setContentType("application/vnd.ms-excel");
@@ -33,7 +59,7 @@ public class EasyExcelUtil {
             String fileName = URLEncoder.encode("teacher-detail", "UTF-8").replaceAll("\\+", "%20");
             response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + fileName + ".xlsx");
             // 这里需要设置不关闭流
-            EasyExcel.write(response.getOutputStream(), Student.class).autoCloseStream(Boolean.FALSE).sheet("模板")
+            EasyExcel.write(response.getOutputStream(), Student.class).autoCloseStream(Boolean.FALSE).sheet("模板").registerWriteHandler(horizontalCellStyleStrategy)
                     .doWrite(data);
 
         } catch (Exception e) {
